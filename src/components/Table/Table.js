965987cb2f5384735fb27a05ca97ed63dbe3985b
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2022-03-29 17:09:29 +0300

    extended Table with available cars

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,18 +4,32 @@ import { MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
 import { Link } from "react-router-dom";
 
 function Table(props) {
+  console.log(props.data);
+  console.log(
+    " item.model.vehicleEquip.map((k) => k.speed)",
+    props.data.map((i) => i.model.vehicleEquip.map((k) => k.speed))
+  );
   return (
     <div className="container-table">
       <MDBTable autoWidth responsive>
         <MDBTableHead>
           <tr>
-            <th ref={props.titleRefCarsCreateToTable}>№ Автомобиля</th>
+            <th ref={props.titleRefCarsCreateToTable}>№</th>
             <th ref={props.titleRefCarsEditToTable}>Фото</th>
             <th>Модель</th>
             <th>Цвет</th>
             <th>Цена</th>
             <th>Год выпуска</th>
             <th>Доступность</th>
+            <th>Скорость</th>
+            <th>Мощность, л.с.</th>
+            <th>Крутящийся момент</th>
+            <th>Объем двигателя, л.</th>
+            <th>Время разгона</th>
+            <th>Смешанный цикл</th>
+            <th>Объем багажника</th>
+            <th>Объем топливного бака, л.</th>
+            <th>Рекомендуемое топливо</th>
             {props.role === 2 && <th></th>}
             {props.role === 2 && <th></th>}
           </tr>
@@ -39,9 +53,18 @@ function Table(props) {
                 <td>{item.color.color1}</td>
                 <td>{item.price}</td>
                 <td>{item.releaseYear}</td>
-
                 <td>{item.availability ? "на складе" : "отсутствует"}</td>
-
+                <td>{item.model.vehicleEquip.map((k) => k.speed)}</td>
+                <td>{item.model.vehicleEquip.map((k) => k.hp)}</td>
+                <td>{item.model.vehicleEquip.map((k) => k.torque)}</td>
+                <td>{item.model.vehicleEquip.map((k) => k.equipType)}</td>
+                <td>
+                  {item.model.vehicleEquip.map((k) => k.accelerationTime)}
+                </td>
+                <td>{item.model.vehicleEquip.map((k) => k.mixedCycle)}</td>
+                <td>{item.model.vehicleEquip.map((k) => k.trunkVolume)}</td>
+                <td>{item.model.vehicleEquip.map((k) => k.fuelCapacity)}</td>
+                <td>{item.model.vehicleEquip.map((k) => k.recommendedFuel)}</td>
                 {props.role === 2 && (
                   <td>
                     {" "}
